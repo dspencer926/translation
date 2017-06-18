53e0c2414c8b7b776fdaf8cc201d10a7e3990287
david spencer <dspencer926@gmail.com>
2017-06-17 21:53:34 -0400

    comment out model

--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
-const db = require('../db/config');
+// const db = require('../db/config');
 
-const User = {};
+// const User = {};
 
-User.findByUserName = userName => {
-  return db.oneOrNone('SELECT * FROM users WHERE username = $1', [userName])
-};
+// User.findByUserName = userName => {
+//   return db.oneOrNone('SELECT * FROM users WHERE username = $1', [userName])
+// };
 
-User.create = user => {
-  console.log('in model')
-  return db.one(
-    `
-      INSERT INTO users 
-      (username, password)
-      VALUES ($1, $2) RETURNING *
-    `, [user.username, user.password]
-  )
-}
+// User.create = user => {
+//   console.log('in model')
+//   return db.one(
+//     `
+//       INSERT INTO users 
+//       (username, password)
+//       VALUES ($1, $2) RETURNING *
+//     `, [user.username, user.password]
+//   )
+// }
 
-module.exports = User;
+// module.exports = User;
